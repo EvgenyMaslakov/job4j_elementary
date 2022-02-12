5002036699f3fa7d70c9d4a713a95a72a511c92f
Jeka <masl.ev@mail.ru>
2022-02-12 23:42:59 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,46 +7,37 @@ public class PointTest {
 
     @Test
     public void when00to20then2() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double dist = a.distance(b);
         double expected = 2;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when26to48then2dot83() {
-        int x1 = 2;
-        int y1 = 6;
-        int x2 = 4;
-        int y2 = 8;
+        Point a = new Point(2, 6);
+        Point b = new Point(4, 8);
+        double dist = a.distance(b);
         double expected = 2.83;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when12to56then5dot66() {
-        int x1 = 1;
-        int y1 = 2;
-        int x2 = 5;
-        int y2 = 6;
+        Point a = new Point(1, 2);
+        Point b = new Point(5, 6);
+        double dist = a.distance(b);
         double expected = 5.66;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when31to94then6dot71() {
-        int x1 = 3;
-        int y1 = 1;
-        int x2 = 9;
-        int y2 = 4;
+        Point a = new Point(3, 1);
+        Point b = new Point(9, 4);
+        double dist = a.distance(b);
         double expected = 6.71;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Assert.assertEquals(expected, dist, 0.01);
     }
-
 }
