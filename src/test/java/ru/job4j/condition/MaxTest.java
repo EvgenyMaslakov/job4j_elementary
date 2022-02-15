71169d65188bcbb7ffc7f637fc65d8364dbc2f3d
Jeka <masl.ev@mail.ru>
2022-02-15 19:32:41 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,29 +6,50 @@ import org.junit.Test;
 public class MaxTest {
 
     @Test
-    public void whenLeft5Right10then10() {
-        int left = 5;
-        int right = 10;
+    public void whenFirstt5Second10then10() {
+        int first = 5;
+        int second = 10;
         int expected = 10;
-        int out = Max.max(left, right);
+        int out = Max.max(first, second);
         Assert.assertEquals(expected, out);
     }
 
     @Test
-    public void whenLeft10Right5then10() {
-        int left = 10;
-        int right = 5;
+    public void whenFirst10Second5then10() {
+        int first = 10;
+        int second = 5;
         int expected = 10;
-        int out = Max.max(left, right);
+        int out = Max.max(first, second);
         Assert.assertEquals(expected, out);
     }
 
     @Test
-    public void whenLeft10Right10then10() {
-        int left = 10;
-        int right = 10;
+    public void whenFirst10Second10then10() {
+        int first = 10;
+        int second = 10;
         int expected = 10;
-        int out = Max.max(left, right);
+        int out = Max.max(first, second);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenFirst10Second5Third7then10() {
+        int first = 10;
+        int second = 5;
+        int third = 7;
+        int expected = 10;
+        int out = Max.max(first, second, third);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenFirst10Second5Third7Fourth11then10() {
+        int first = 10;
+        int second = 5;
+        int third = 7;
+        int fourth = 11;
+        int expected = 11;
+        int out = Max.max(first, second, third, fourth);
         Assert.assertEquals(expected, out);
     }
 }
