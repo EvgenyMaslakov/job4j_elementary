d2d7307ce5084618a4a40c5dd194c615c68a10e3
Евгений <masl.ev@mail.ru>
2023-02-24 15:42:13 +0300

    Task9

---
+++ src/main/java/ru/job4j/type/Task9.java
@@ -0,0 +1,29 @@
+package ru.job4j.type;
+
+import java.util.Scanner;
+
+/*
+Даны два действительных числа a и b. Вычислить их сумму, разность,
+произведение и частное.
+
+Результат вычислений выведите на консоль в порядке следование.
+
+- сумма
+
+- разность
+
+- произведение
+
+- часное
+ */
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+    }
+}
