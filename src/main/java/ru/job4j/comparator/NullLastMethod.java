7a196ffec40237545af391a66f581c566cf4ee26
Евгений <masl.ev@mail.ru>
2023-06-02 09:20:52 +0300

    Сортировка

---
+++ src/main/java/ru/job4j/comparator/NullLastMethod.java
@@ -0,0 +1,27 @@
+package ru.job4j.comparator;
+
+import java.util.Comparator;
+
+/**
+ * 1.3.4. Сортировка
+ * 1.4. Получение компаратора. Метод nullsLast()
+ *
+ * Если мы хотим, что бы null элементы скопились в конце мы можем использовать компаратор, созданный с
+ * помощью метода Comparator.nullsLast(). Он аналогичен nullsFirst() за исключением, что null элементы
+ * при его применении скопятся в конце.
+ *
+ * Например:
+ *
+ * Comparator<Integer> comparator = Comparator.nullsLast(Comparator.naturalOrder());
+ *
+ * Сначала создастся компаратор, у которого будет использован метод compare(), а только потом компаратор,
+ * который учитывает null
+ *
+ * Ваша задача получить null-frendly компаратор для строки.
+ */
+public class NullLastMethod {
+    public static Comparator<String> nullLast() {
+        Comparator<String> comparator = Comparator.nullsLast(Comparator.naturalOrder());
+        return comparator;
+    }
+}
