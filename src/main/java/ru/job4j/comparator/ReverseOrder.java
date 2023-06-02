aab495b1f2f3931a2601458d9fe9942748c3725b
Евгений <masl.ev@mail.ru>
2023-06-02 09:07:13 +0300

    Сортировка

---
+++ src/main/java/ru/job4j/comparator/ReverseOrder.java
@@ -0,0 +1,21 @@
+package ru.job4j.comparator;
+
+import java.util.Comparator;
+
+/**
+ * 1.3.4. Сортировка
+ * 1.2. Получение компаратора. Метод reverseOrder()
+ *
+ * Если нам нужно получить порядок элементов обратный naturalOrder(), можно использовать метод
+ * Comparator.reverseOrder(). Этот метод также использует реализацию метода compareTo().
+ *
+ * Например, если реализация такая o1.compareTo(o2), то метод reverseOrder() вернет компаратор, основанный
+ * на сравнении o2.compareTo(o1).
+ *
+ * Ваша задача получить обратный компаратор для строки
+ */
+public class ReverseOrder {
+    public static Comparator<String> reverseOrder() {
+         return Comparator.reverseOrder();
+    }
+}
