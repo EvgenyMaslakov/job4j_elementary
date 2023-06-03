c754aff28d9b66b67727e7d6a7dd117c0413ada2
Евгений <masl.ev@mail.ru>
2023-06-03 12:30:43 +0300

    Сортировка

---
+++ src/main/java/ru/job4j/comparator/ArraysSort.java
@@ -0,0 +1,25 @@
+package ru.job4j.comparator;
+
+import java.util.Arrays;
+
+/**
+ * 1.3.4. Сортировка
+ * 2.1. Способы сортировки. Сортировка массива примитивов
+ *
+ * Иногда нужно бывает отсортировать массив. Для этого есть метод void Arrays.sort(массив),
+ * который сортирует примитивы в восходящем порядке.
+ *
+ * Например,
+ *
+ * int[] data = {3, 2, 1};
+ *
+ * Arrays.sort(data); // теперь массив будет {1, 2, 3}
+ *
+ * Ваша задача отсортировать переданный массив и вернуть его
+ */
+public class ArraysSort {
+    public static long[] sort(long[] data) {
+        Arrays.sort(data);
+        return data;
+    }
+}
