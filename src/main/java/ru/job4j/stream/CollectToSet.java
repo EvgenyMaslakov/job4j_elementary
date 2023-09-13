e20b03c1ef6afc95ff5b35c00ed2b7237f91ee53
Евгений <masl.ev@mail.ru>
2023-09-13 07:54:58 +0300

    1.4.2. Stream API 3.1. Сборка элементов. Сборка в Set

---
+++ src/main/java/ru/job4j/stream/CollectToSet.java
@@ -0,0 +1,24 @@
+package ru.job4j.stream;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 1.4.2. Stream API
+ * 3.1. Сборка элементов. Сборка в Set
+ * 3.1. Сборка элементов. Сборка в Set
+ * Для того чтобы собрать данные в сет, мы можем использовать методы collect()
+ * и Collectors.toSet(). Сборка идет в HashSet
+ *
+ * Например,
+ *
+ * Set<Integer> result = Set.of(1, 2).stream().collect(Collectors.toSet());
+ *
+ * Ваша задача собрать, переданный стрим в сет
+ */
+public class CollectToSet {
+    public static Set<Integer> collect(Stream<Integer> data) {
+        return data.collect(Collectors.toSet());
+    }
+}
