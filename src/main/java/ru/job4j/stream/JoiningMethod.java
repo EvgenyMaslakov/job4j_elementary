c47540d7532b5208896bfa3713b7461372171fe6
Евгений <masl.ev@mail.ru>
2023-09-14 07:09:56 +0300

    1.4.2. Stream API 3.5. Сборка элементов. Сборка в строку

---
+++ src/main/java/ru/job4j/stream/JoiningMethod.java
@@ -0,0 +1,37 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 1.4.2. Stream API
+ * 3.5. Сборка элементов. Сборка в строку
+ * 3.5. Сборка элементов. Сборка в строку
+ * Для сборки текста мы использовали StringJoiner, или StringBuilder.
+ * Стримы также позволяют сложить элементы, если они являются строками,
+ * в единый текст.
+ * Для этого используется метод collect() и Collectors.joining().
+ * Метод joining() принимает те же аргументы, что и StringJoiner
+ *
+ * Пример,
+ *
+ * List.of("A", "B", "C").stream().collect(Collectors.joining(System.lineSeparator()));
+ *
+ * Получим
+ *
+ * A
+ *
+ * B
+ *
+ * C
+ *
+ * Ваша задача собрать строки списка в текст и добавить префикс "Prefix",
+ * и суффикc "Suffix"(т.е. нужно использовать перегруженный вариант joining()).
+ * Разделителем должен быть System.lineSeparator()
+ */
+public class JoiningMethod {
+    public static String join(List<String> strings) {
+        return strings.stream().collect(Collectors.joining(System.lineSeparator(),
+                "Prefix", "Suffix"));
+    }
+}
