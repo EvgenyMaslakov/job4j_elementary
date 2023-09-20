5b94fc9d64ee302218906d2c46979f01f995baf3
Евгений <masl.ev@mail.ru>
2023-09-20 21:04:04 +0300

    1.4.2. Stream API 6.18. Промежуточные операции. Метод dropWhile()

---
+++ src/main/java/ru/job4j/stream/DropWhileMethod.java
@@ -0,0 +1,21 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 1.4.2. Stream API
+ * 6.18. Промежуточные операции. Метод dropWhile()
+ *
+ * Данный метод похож на метод takeWhile() только он позволяет пропустить элементы, которые удовлетворяют условию.
+ * Этот метод также нужно использовать только для отсортированных элементов, т.к. если для первого элемента
+ * не выполниться условие, то в стриме останутся все элементы.
+ *
+ * Ваша задача оставить в списке только положительные элементы.
+ * Порядок элементов исходного списка может быть произвольным
+ */
+public class DropWhileMethod {
+    public static List<Integer> dropWhile(List<Integer> numbers) {
+        return numbers.stream().sorted().dropWhile(number -> number > 0).collect(Collectors.toList());
+    }
+}
