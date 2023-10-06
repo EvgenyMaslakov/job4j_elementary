be82e792d5c4f903be83e0f426c99ce19010324e
Евгений <masl.ev@mail.ru>
2023-10-06 07:12:33 +0300

    1.1.2. Типы данных 4.3. Вызов метода без указания класса

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,14 +1,24 @@
 package ru.job4j.calculator;
 
+/**
+ * 1.1.2. Типы данных
+ * 4.3. Вызов метода без указания класса
+ */
 public class Calculator {
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int result = one + two;
-        System.out.println(result);
-        one = 10;
-        two = 11;
-        result = one + two;
-        System.out.println(result);
+        plus(1, 2);
+        plus(10, 11);
+        minus(7, 3);
+        minus(2, 6);
     }
 }
